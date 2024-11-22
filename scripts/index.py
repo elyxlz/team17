89948eadfcf1a52c4ba10c55009e20c0db124387
elyx <elio@pascarelli.com>
2024-11-22 12:49:50 +0000

    scripts

---
+++ scripts/index.py
@@ -0,0 +1,4 @@
+from voxtral.data.indexing import IndexConfig, index_youtube_urls
+
+config = IndexConfig()
+index_youtube_urls(config)
---
+++ scripts/scrape.py
@@ -0,0 +1,4 @@
+from voxtral.data.scraping import ScrapingConfig, scrape_youtube_urls
+
+config = ScrapingConfig()
+scrape_youtube_urls(config)
