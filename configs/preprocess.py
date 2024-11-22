c2678f28717c1c43a3f777218a8f8ab0ff7c23e5
elyx <elio@pascarelli.com>
2024-11-22 20:58:47 +0000

    better whisperx?

--- configs/preprocess.py
+++ configs/preprocess.py
@@ -1,4 +1,4 @@
 from team17.data.preprocessing import PreprocessingConfig, process_audio_chunks
 
-config = PreprocessingConfig()
+config = PreprocessingConfig(chunk_frames=16_000 * 10)
 process_audio_chunks(config)
--- configs/scrape.py
+++ configs/scrape.py
@@ -1,4 +1,4 @@
 from team17.data.scraping import ScrapingConfig, scrape_youtube_urls
 
-config = ScrapingConfig(max_workers=2)
+config = ScrapingConfig(max_workers=2, chunk_duration=40)
 scrape_youtube_urls(config)
